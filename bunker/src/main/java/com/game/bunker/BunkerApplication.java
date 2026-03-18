45e42c1c48b4fe0bc0c748d2ce95c46be4663bd0
Dmitriy Demchuk <smildemchuk130206@gmail.com>
2026-03-18 21:30:25 +0900

    Initial commit

---
+++ bunker/src/main/java/com/game/bunker/BunkerApplication.java
@@ -0,0 +1,13 @@
+package com.game.bunker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunkerApplication.class, args);
+	}
+
+}
---
+++ bunker/src/test/java/com/game/bunker/BunkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.bunker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
