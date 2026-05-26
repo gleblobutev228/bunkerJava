f975bfeeeb58359fdf20601cdd60a10e7a457a2b
Dmitriy Demchuk <smildemchuk130206@gmail.com>
2026-05-27 01:01:55 +0900

    н

--- bunker/src/main/java/com/game/bunker/dto/StartGameResponse.java
+++
@@ -1,9 +0,0 @@
-package com.game.bunker.dto;
-
-import com.game.bunker.entity.Lobby;
-
-public record StartGameResponse(
-        String token,
-        Lobby lobby
-) {
-}
--- bunker/src/main/java/com/game/bunker/dto/ws/PersonalGameDataMessage.java
+++
@@ -1,9 +0,0 @@
-package com.game.bunker.dto.ws;
-
-import com.game.bunker.entity.User;
-
-public record PersonalGameDataMessage(
-        String token,
-        User user
-) {
-}
